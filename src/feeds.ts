80268d932ae98e128f6332afe3db29f434981b23
Joe Chen <jc@unknwon.io>
2024-09-29 20:17:18 -0400

    Update feeds.ts

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,10 +1,4 @@
 export const feeds = [
-  // Daring Fireball
-  'https://daringfireball.net/feeds/main',
-
-  // JakeArchibald.com
-  'https://jakearchibald.com/posts.rss',
-
-  // Appjeniksaan
-  'https://appjeniksaan.nl/feed.xml',
+  // Lucas F. Costa
+  'https://lucasfcosta.com/feed.xml',
 ]
