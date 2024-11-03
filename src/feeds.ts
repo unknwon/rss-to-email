d48f9f3452ccc5320839c5d131770163b0692057
Joe Chen <jc@unknwon.io>
2024-11-03 14:33:39 -0500

    Update feeds.ts

--- src/feeds.ts
+++ src/feeds.ts
@@ -5,4 +5,6 @@ export const feeds = [
   'https://feeds.feedburner.com/blogspot/amDG',
   // 面向信仰编程
   'https://draveness.me/feed.xml',
+  // CrunchyData Blog
+  'https://www.crunchydata.com/blog/rss.xml',
 ]
