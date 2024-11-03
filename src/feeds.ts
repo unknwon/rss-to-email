b9eb328251d19ca265cb7a02c08180008845a95d
Joe Chen <jc@unknwon.io>
2024-11-03 14:32:18 -0500

    Update feeds.ts

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,7 +1,8 @@
 export const feeds = [
   // Lucas F. Costa
   'https://lucasfcosta.com/feed.xml',
-
   // Google Search Central Blog
   'https://feeds.feedburner.com/blogspot/amDG',
+  // 面向信仰编程
+  'https://draveness.me/feed.xml',
 ]
