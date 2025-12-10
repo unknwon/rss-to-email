e6066a2e0c39e4d2653faf3ec744ddf6e2b09fbb
ᴊᴏᴇ ᴄʜᴇɴ <jc@unknwon.io>
2025-12-10 18:11:29 -0500

    Comment out Google Search Central Blog feed URL
    Commented out the Google Search Central Blog feed URL.

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,8 +1,8 @@
 export const feeds = [
   // Lucas F. Costa
   'https://lucasfcosta.com/feed.xml',
-  // Google Search Central Blog
-  'https://feeds.feedburner.com/blogspot/amDG',
+  // Google Search Central Blog - erroring
+  // 'https://feeds.feedburner.com/blogspot/amDG',
   // 面向信仰编程
   'https://draveness.me/feed.xml',
   // CrunchyData Blog
